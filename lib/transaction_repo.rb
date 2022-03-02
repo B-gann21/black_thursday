bbf77c6ee6a5b8c44e37d82bedd5f0383cf0fad9
Drew Proebstel <d.proebstel@gmail.com>
2022-03-02 10:29:07 -0800

    find all by invoice id

--- lib/transaction_repo.rb
+++ lib/transaction_repo.rb
@@ -8,13 +8,17 @@ class TransactionRepository
   def inspect
     "#<#{self.class} #{@all.size} rows>"
   end
-  
+
   def initialize(transactions)
     @all = transactions
   end
 
   def find_all_by_id(id_search)
     @all.find_all{|index| index[:id].to_s.include?(id_search.to_s)}
+  end
+
+  def find_all_by_invoice_id(invoice_id_search)
+    @all.find_all{|index| index.invoice_id == (invoice_id_search)}
   end
 
   def find_all_by_credit_card_number(card_search)
