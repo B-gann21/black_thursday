220de28fc56cfa52344a576daf0efd16c1f8e887
Drew Proebstel <d.proebstel@gmail.com>
2022-03-02 10:00:27 -0800

    updated invoic item

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -3,9 +3,9 @@ class InvoiceItem
   attr_reader :id, :item_id, :invoice_id, :quantity,
               :unit_price, :created_at, :updated_at
   def initialize(info)
-    @id = info[:id]
-    @item_id = info[:item_id]
-    @invoice_id = info[:invoice_id]
+    @id = info[:id].to_i
+    @item_id = info[:item_id].to_i
+    @invoice_id = info[:invoice_id].to_i
     @quantity = info[:quantity]
     @unit_price = info[:unit_price]
     @created_at = info[:created_at]
