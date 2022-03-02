bd8fed1c5371ab955d0a0d27541aecf37c542921
Drew Proebstel <d.proebstel@gmail.com>
2022-03-02 10:05:54 -0800

    find all invoice by item

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -9,6 +9,10 @@ class InvoiceItemRepository
   def find_all_by_invoice_id(invoice_id)
     @all.find_all { |item| item.invoice_id == invoice_id}
   end
+
+  def find_all_by_item_id(item_id)
+    @all.find_all { |item| item.item_id == item_id}
+  end
   #inspect method is required for spec harness to run
   def inspect
     "#<#{self.class} #{@all.size} rows>"
