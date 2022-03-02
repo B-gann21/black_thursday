80a8b3d8e7114ba7a68b1e269956721665d48d2d
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-02 12:22:40 -0700

    Final require statement added for simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'simplecov'
 SimpleCov.start
 require "./spec/sales_engine_spec"
+require './spec/sales_analyst_spec'
 require "./spec/item_spec"
 require './spec/item_repository_spec.rb'
 require "./spec/merchant_repository_spec"
